5735febf9f35aaabf755602992a4c239f9144c98
Brandon <bartymowycz@gmail.com>
2023-02-17 14:33:23 -0500

    Defining a data type and a collection

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+// Slice of pointers to instances of the Rsvp struct
+var responses = make([]*Rsvp, 0, 10)
+
 func main() {
 
 }
